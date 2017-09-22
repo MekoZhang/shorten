4e7fd0369cf54346084a6238645d6cc86b5c4ad5
Sheldon <zhangxd_2011sz@163.com>
2017-09-22 14:41:05 +0800

    add main

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/zhangxd1989/shorten/conf"
+	"github.com/zhangxd1989/shorten/short"
+	"github.com/zhangxd1989/shorten/web"
+)
+
+func main() {
+	cfgFile := flag.String("c", "config.conf", "configuration file")
+	version := flag.Bool("v", false, "Version")
+
+	flag.Parse()
+
+	if *version {
+		fmt.Println(conf.Version)
+		os.Exit(0)
+	}
+
+	// parse config
+	conf.ParseConfig(*cfgFile)
+
+	// short service
+	short.Start()
+
+	// api
+	web.Start()
+}
